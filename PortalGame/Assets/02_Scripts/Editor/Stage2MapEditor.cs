0eb98a2675b900e6f2751ebd483fcb936b7b59c7
songyechan1007 <songyechan1007@gmail.com>
2023-10-09 21:25:02 +0900

    MapManager

--- PortalGame/Assets/02_Scripts/Editor/Stage2MapEditor.cs
+++ PortalGame/Assets/02_Scripts/Editor/Stage2MapEditor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea46ca91c2cd1ae3f2b86057c49168bd5f6f3e0c959df62e143bd0ff77eba6c8
-size 8832
+oid sha256:04dcc7c9e8562991d9f3b177a4409fbb4d80ed1bba24ea42c693bce7eee267bb
+size 9391
---
+++ PortalGame/Assets/02_Scripts/Map/MapManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:410802da5ef6dd6ab50baa1fed39280750a3c4b75e10e181916ae8a8580529e1
+size 212
