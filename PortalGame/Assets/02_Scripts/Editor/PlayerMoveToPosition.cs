4a29bc4130165cd30b31b411cc66d21788f40206
songyechan1007 <songyechan1007@gmail.com>
2023-10-11 18:24:02 +0900

    modify

---
+++ PortalGame/Assets/02_Scripts/Editor/PlayerMoveToPosition.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:744bbe57278ddae66b3666c275355cebbcec65498815c5e192390baca5f907b2
+size 1273
--- PortalGame/Assets/02_Scripts/Player/PortalCreate_Stage2.cs
+++ PortalGame/Assets/02_Scripts/Player/PortalCreate_Stage2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af995013a6a13d9d5b77c5b756cd7cb575f6b0b444b6af3d8d93ee757398d11c
-size 14626
+oid sha256:cbbdf7efef4e85cc0bb17c97b67a340f5dd9b98deca6e5e9f9db946662f443f4
+size 21681
