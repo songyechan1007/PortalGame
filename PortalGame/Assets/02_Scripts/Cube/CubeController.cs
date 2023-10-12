3ad0e6d6995f0721abf2fbb92cb401ad961ffd61
songyechan1007 <songyechan1007@gmail.com>
2023-10-12 18:22:34 +0900

    Object 상호작용

---
+++ PortalGame/Assets/02_Scripts/Cube/CubeController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f1ccaf19ebf9aabfb6a37c61ae3679d4e5d5614435e70d0eb4355d5cb77c2eb
+size 587
---
+++ PortalGame/Assets/02_Scripts/Player/MoveCube.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3af70498d7ef97194313fecc3218576c951c527af0c6164baadca215fbad53fe
+size 1100
